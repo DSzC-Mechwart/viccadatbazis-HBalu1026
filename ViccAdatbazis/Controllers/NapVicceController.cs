3c2a70ccc56e505e7c5e671b6bae47a8081d9f64
hernadi.balazs <hernadi.balazs@BURG-1E06-20>
2024-10-09 09:08:43 +0200

    valami kész

---
+++ ViccAdatbazis/Controllers/NapVicceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViccAdatbazis.Controllers
+{
+    public class NapVicceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[controller]")]
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
