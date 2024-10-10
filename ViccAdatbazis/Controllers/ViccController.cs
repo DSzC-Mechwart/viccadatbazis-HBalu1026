a57eda98d2835262bc37c1d8a5abfa1c12d18d67
hernadi.balazs <hernadi.balazs@BURG-1E05-17>
2024-10-10 09:02:33 +0200

    aha

---
+++ ViccAdatbazis/Controllers/ViccController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViccAdatbazis.Data;
+using ViccAdatbazis.Models;
+
+namespace ViccAdatbazis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViccController : ControllerBase
+    {
+        //adatbázis kapcsolat
+        private readonly ViccDbContext _context;
+        public ViccController(ViccDbContext context)
+        {
+            _context = context;
+        }
+        //összes vicc lekérdezése
+        [HttpGet]
+        //public ActionResult<List<Vicc>> GetViccek() 
+        //{ 
+        //    return _context.Viccek.Where(x => x.Aktiv == true).ToList();
+        //}
+        //asszinkron mód
+        public async Task<ActionResult<List<Vicc>>> GetViccek()
+        {
+            return await _context.Viccek.Where(x => x.Aktiv == true).ToListAsync();
+        }
+
+        //egy vicc lekérdezése
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vicc>> GetVicc(int id)
+        {
+            var vicc = await _context.Viccek.FindAsync(id);
+            return vicc == null ? NotFound() : vicc;
+        }
+
+        //vicc feltöltése
+        [HttpPost]
+        public async Task<ActionResult<Vicc>> PostVicc(Vicc vicc)
+        {
+            _context.Viccek.Add(vicc);
+            await _context.SaveChangesAsync();
+            //return Ok();
+            return CreatedAtAction("GetVicc", new { id = vicc.Id }, vicc);
+        }
+
+        //vicc módosítása
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutVicc(int id, Vicc vicc)
+        {
+            if(id != vicc.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(vicc).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        //vicc törlése
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteVicc(int id)
+        {
+            var vicc = await _context.Viccek.FindAsync(id);
+            if(vicc == null)
+            {
+                return BadRequest();
+            }
+            if(vicc.Aktiv == true)
+            {
+                vicc.Aktiv = false;
+            }
+            else
+            {
+                _context.Viccek.Remove(vicc);
+            }
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
